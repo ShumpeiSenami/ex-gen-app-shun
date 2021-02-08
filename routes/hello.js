cdc7cd5f86026f1f5e58ca065c8731429b79f9d2
Shumpei Senami <kaifenan@gmail.com>
2021-02-08 09:44:09 +0900

    update処理の追加

--- routes/hello.js
+++ routes/hello.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { MemoryStore } = require('express-session');
 var router = express.Router();
 
 var mysql = require('mysql');
@@ -69,6 +70,41 @@ router.get('/show', (req, res, next) => {
     }
   });
   connecttion.end();
+});
+
+router.get('/edit', (req, res, next) => {
+  var id = req.query.id;
+  var connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+  connection.query('SELECT * from mydata where id=?', id,
+          function (error, results, fields) {
+          if (error == null) {
+          var data = {
+              title: 'Hello/edit',
+              content: 'id = ' + id + ' のレコード：',
+              mydata: results[0]
+          }
+          res.render('hello/edit', data);
+          console.log(data);
+      }
+  });
+  connection.end();
+});
+
+router.post('/edit', (req, res, next) => {
+  var id = req.body.id;
+  var nm = req.body.name;
+  var ml = req.body.mail;
+  var ag = req.body.age;
+  var data = {'name':nm, 'mail':ml, 'age':ag};
+  var connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+  connection.query('update mydata set ? where id=?',
+    [data, id], function(error, results, fields){
+    console.log("id" + id);
+    res.redirect('/hello');
+  });
+  connection.end();
 });
 
 module.exports = router;
