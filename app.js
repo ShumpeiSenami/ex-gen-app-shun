d56e91c5bbaad473b1652938c7c56ce4b6b2b798
Shumpei Senami <kaifenan@gmail.com>
2021-02-10 12:40:10 +0900

    /helloをBookshelfで修正

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var validator = require('express-validator');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/hello.js
+++ routes/hello.js
@@ -1,9 +1,27 @@
 var express = require('express');
 const { MemoryStore } = require('express-session');
+const { Result } = require('express-validator');
 const { NotImplemented } = require('http-errors');
 var router = express.Router();
 
+
 var mysql = require('mysql');
+// require('knex)({初期設定のデータ});
+var knex = require('knex')({
+  dialect: 'mysql',     // 使用するデータベースの種類
+  connection: {         // 接続するデータベースに関する設定情報をまとめたもの
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'my-nodeapp-db',
+    charset: 'utf8'
+  }
+});
+var Bookshelf = require('bookshelf')(knex);
+// モデルとはデータベースにあるテーブルを扱うためのオブジェクト
+var MyData = Bookshelf.Model.extend({  //引数にモデルに関する設定情報をまとめたオブジェクトを用意
+  tableName: 'mydata'
+})
 const { route } = require('.');
 
 var mysql_setting = {
@@ -14,44 +32,73 @@ var mysql_setting = {
 };
 
 
-router.get('/',(req, res, next) => {
-  var connecttion = mysql.createConnection(mysql_setting);
-  connecttion.connect();
-
-  connecttion.query('SELECT * from mydata',
-    function(error, results, fields){
-      // 発生したerrorがnullであるならば真=エラーがなければ
-      if(error == null){
-        var data = {title:'mysql', content:results};
-        res.render('hello/index', data);
-      }
-    });
-
-    connecttion.end();
+router.get('/', (req, res, next) => {
+  // fetchAll は全てのレコードを取得する .thenはコールバック関数
+  new MyData().fetchAll().then((collection) => {
+      var data = {
+                  title: 'Hello!',
+                  content: collection.toArray()
+              };
+              res.render('hello/index', data);
+  })
+  .catch((err) => {
+      res.status(500).json({error: true, data: {message: err.message}});
+  });
 });
 
+// 新規作成ページへのアクセス
 router.get('/add', (req, res, next) => {
   var data = {
-    title: 'Hello/Add',
-    content: '新しいレコードを入力：'
+      title: 'Hello/Add',
+      content: '新しいレコードを入力：',
+      form: {name:'', mail:'', age:0}
   }
   res.render('hello/add', data);
 });
 
+
+// 新規作成フォームの送信処理
 router.post('/add', (req, res, next) => {
-  var nm = req.body.name;
-  var ml = req.body.mail;
-  var ag = req.body.age;
-  var data = {'name':nm, 'mail':ml, 'age':ag};
+  // ('チェック項目', 'エラ-メッセージ')
+  req.check('name','NAME は必ず入力して下さい。').notEmpty();
+  req.check('mail','MAIL はメールアドレスを記入して下さい。').isEmail();
+  req.check('age', 'AGE は年齢（整数）を入力下さい。').isInt();
 
-  var connecttion = mysql.createConnection(mysql_setting);
-  connecttion.connect();
-
-  connecttion.query('insert into mydata set ?', data, function(error, results, fields){
-    res.redirect('/hello');
+  // バリデーションの結果を取得し非同期でバリデーション処理を実行しthenにコールバック関数を用意
+  req.getValidationResult().then((result) => {
+      // 受け取ったバリデーションが空でないならtrue（バリデーションのチェックに引っ掛かった場合）
+      if (!result.isEmpty()) {
+          var re = '<ul class="error">';
+          // 受け取ったエラーメッセージを配列として取り出す
+          var result_arr = result.array();
+          // 配列を変数 n　に取り出してエラーメッセージの表示を作成
+          for(var n in result_arr) {
+              re += '<li>' + result_arr[n].msg + '</li>'
+          }
+          re += '</ul>';
+          var data = {
+              title: 'Hello/Add',
+              content: re,
+              form: req.body
+          }
+          res.render('hello/add', data);
+      } else {
+          var nm = req.body.name;
+          var ml = req.body.mail;
+          var ag = req.body.age;
+          var data = {'name':nm, 'mail':ml, 'age':ag};
+          
+          var connection = mysql.createConnection(mysql_setting);
+          connection.connect();
+          connection.query('insert into mydata set ?', data, 
+                  function (error, results, fields) {
+              res.redirect('/hello');
+          });
+          connection.end();
+      }
   });
-  connecttion.end();
 });
+
 
 router.get('/show', (req, res, next) => {
   var id = req.query.id;
